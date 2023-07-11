c3ff96bdf69aed2e1d912684dc2676cef32dd7cc
ricky <zhangqingcq@foxmail.com>
2023-07-11 15:35:50 +0800

    添加样式

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@
  * @author Ricky email:zhangqingcq@foxmail.com
  * @created 2023.07.10
  */
+
+import 'virtual:windi.css'
+import 'virtual:svg-icons-register'
+import './assets/main.less'
+
 import SvgEditor from '@/components/svg-editor/index.vue'
 import SvgViewer from '@/components/svg-viewer/index.vue'
 
